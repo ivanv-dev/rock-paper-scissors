3854df38acb4868790732ffc532c99c694e9ff5e
ivan.vukicevic <ivan.vukicevic.dev@gmail.com>
2022-03-13 18:46:44 +0100

    5 rounds insteaded of 1 & draw possible

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ else if (randomNumber <= 0.66) return paper;
 else return scissors;
 }
 
-function winMessage(firstItem, secondItem){
+function win(firstItem, secondItem){
+    playerPoints++;
     return `You won! --- ${firstItem} beats ${secondItem}`;
 }
 
-function loseMessage(firstItem, secondItem){
+function lose(firstItem, secondItem){
+    computerPoints++;
     return `You lost! --- ${firstItem} beats ${secondItem}`;
 }
 
@@ -35,15 +37,15 @@ else return getOutput(computerSelection, playerSelectionLowercased);
 }
 
 function winOutput(computerSelection, playerSlection){
-if(playerSlection === paper && computerSelection === rock) return winMessage(paper, rock);
-else if(playerSlection === scissors && computerSelection === paper) return winMessage(scissors, paper);
-else if(playerSlection === rock && computerSelection === scissors) return winMessage(rock, scissors);
+if(playerSlection === paper && computerSelection === rock) return win(paper, rock);
+else if(playerSlection === scissors && computerSelection === paper) return win(scissors, paper);
+else if(playerSlection === rock && computerSelection === scissors) return win(rock, scissors);
 return null;
 }
 function lostOutput(computerSelection, playerSlection){
-if(playerSlection === rock && computerSelection === paper) return loseMessage(paper, rock);
-else if(playerSlection === paper && computerSelection === scissors) return loseMessage(scissors, paper);
-else if(playerSlection === scissors && computerSelection === rock) return loseMessage(rock, scissors);
+if(playerSlection === rock && computerSelection === paper) return lose(paper, rock);
+else if(playerSlection === paper && computerSelection === scissors) return lose(scissors, paper);
+else if(playerSlection === scissors && computerSelection === rock) return lose(rock, scissors);
 return null;
 }
 
@@ -55,9 +57,22 @@ else if( lose !== null) return lose;
 else return 'Wrong input, insert rock, paper or scissors!';
 }
 
-function init(){
+function playRound(){
  const input = prompt('Choose rock, paper or scissors!')
  console.log(play(input));
 }
 
-init();
+function playGame(){
+    for(let i = 0; i < roundNumbers; i++){
+        playRound();
+        console.log(`ROUND: ${i + 1} --- Player points: ${playerPoints}, Computer points: ${computerPoints}`)
+    }
+
+    if(playerPoints > computerPoints) console.log('Player wins!');
+    else if (playerPoints < computerPoints) console.log('Compter wins!');
+    else console.log('Draw.')
+    playerPoints = 0;
+    computerPoints = 0;
+}
+
+playGame();
